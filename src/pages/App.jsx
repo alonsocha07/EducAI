d1d4d8dee7fc32d9d6b7caa3a67d1bce9d217ac0
alonsochaves <achaves@jandgsolutions.com>
2024-01-22 21:29:56 -0600

    logo

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,7 +2,7 @@ import "../css/App.css";
 import { Header } from "../components/Header";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./HomePage";
-import { Chatgpt } from "./chatgpt";
+import { Chatgpt } from "./Chatgpt";
 import { Dalle2 } from "./Dalle2";
 import Footer from "../components/Footer";
 import {PLaygroundContainer} from "../components/PLaygroundContainer";
