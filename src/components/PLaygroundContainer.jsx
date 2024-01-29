f4ca5566ce4d5cfff613e4e83d120d63235c23ce
alonsochaves <achaves@jandgsolutions.com>
2024-01-28 19:34:14 -0600

    solo dejar intro y presi

--- src/components/PLaygroundContainer.jsx
+++ src/components/PLaygroundContainer.jsx
@@ -6,23 +6,7 @@ import "../css/playground.css";
 
 import Precision from './chatgpt/Precision'
 import Introduccion from "./chatgpt/Introduccion"
-import Estructura from "./chatgpt/Estructura"
-import Limites from "./chatgpt/Limites"
-import Informacion_sensible from "./chatgpt/Informacion_sensible"
-import Tips_basicos from "./chatgpt/Tips_basicos"
-import Iniciar_sencillo from "./chatgpt/Iniciar_sencillo"
-import Ser_especifico from "./chatgpt/Ser_especifico";
-import Evitar_ambiguedades from "./chatgpt/Evitar_ambiguedades";
-import Reafirmar from "./chatgpt/Reafirmar";
-import Conocer_la_audiencia_objetivo from "./chatgpt/Conocer_la_audiencia_objetivo";
-import Dar_ejemplo from "./chatgpt/Dar_ejemplo";
-import Errores_comunes from "./chatgpt/Errores_comunes";
-import Texto_demasiado_complejo from "./chatgpt/Texto_demasiado_complejo";
-import Jergas_o_ambiguedades from "./chatgpt/Jergas_o_ambiguedades";
-import Limitaciones_del_modelo from "./chatgpt/Limitaciones_del_modelo";
-import Supuestos from "./chatgpt/Supuestos";
-import Tecnica_promt_engineering from "./chatgpt/Tecnica_promt_engineering";
-import Shots from "./chatgpt/Shots";
+
 
 
 function PLaygroundContainer() {
@@ -76,23 +60,6 @@ function PLaygroundContainer() {
 
               {playground.slug == 'introduccion' ? <Introduccion/> : null}
               {playground.slug == 'precision' ? <Precision/> : null}
-              {playground.slug === 'estructura' && <Estructura />}
-              {playground.slug === 'limites' && <Limites />}
-              {playground.slug === 'informacion_sensible' && <Informacion_sensible />}
-              {playground.slug === 'tips_basicos' && <Tips_basicos />}
-              {playground.slug === 'iniciar_sencillo' && <Iniciar_sencillo />}
-              {playground.slug === 'ser_especifico' && <Ser_especifico />}
-              {playground.slug === 'evitar_ambiguedades' && <Evitar_ambiguedades />}
-              {playground.slug === 'reafirmar' && <Reafirmar />}
-              {playground.slug === 'conocer_la_audiencia_objetivo' && <Conocer_la_audiencia_objetivo />}
-              {playground.slug === 'dar_ejemplo' && <Dar_ejemplo />}
-              {playground.slug === 'errores_comunes' && <Errores_comunes />}
-              {playground.slug === 'texto_demasiado_complejo' && <Texto_demasiado_complejo />}
-              {playground.slug === 'jergas_o_ambiguedades' && <Jergas_o_ambiguedades />}
-              {playground.slug === 'limitaciones_del_modelo' && <Limitaciones_del_modelo />}
-              {playground.slug === 'supuestos' && <Supuestos />}
-              {playground.slug === 'tecnica_promt_engineering' && <Tecnica_promt_engineering />}
-              {playground.slug === 'shots' && <Shots />}
               
 
         {playground.prompt && (
